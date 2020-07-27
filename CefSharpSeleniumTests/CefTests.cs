d0ff779bcbf351d72c0534a9f4c45c3e3b1bc62d
mikhailchub <Mikhail.Chub.mailing@gmail.com>
2020-07-27 14:02:15 +0300

    Add project files.

---
+++ CefSharpSeleniumTests/CefTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace CefDriverTest
+{
+    [TestClass]
+    public class CefTestsWithUpdatedApp
+    {
+        public static IWebDriver driver;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            ChromeOptions options = new ChromeOptions();
+            // Debugging port to communicate with
+            options.DebuggerAddress = "localhost:8081";
+
+            driver = new ChromeDriver(options);
+        }
+
+        [TestMethod]
+        public void PutValuesToInputs()
+        {
+            driver.FindElement(By.Id("inputName")).SendKeys("cef test");
+            driver.FindElement(By.Id("inputEmail")).SendKeys("cef@test.com");
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            driver.Quit();
+        }
+    }
+}
---
+++ CefSharpSeleniumTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CefSharpSeleniumTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CefSharpSeleniumTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("19021eb6-f9fd-4d1b-a0bf-232188fb1a90")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
